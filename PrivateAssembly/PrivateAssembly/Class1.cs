561d0cf3ba77ca097f32d5cd2afc6c056c930707
Rahul-agrawal11 <agrawalrahul11112004@gmail.com>
2025-11-26 11:40:38 +0530

    Add files via upload

---
+++ PrivateAssembly/PrivateAssembly/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrivateAssembly
+{
+    public class Class1
+    {
+            public static string SpaceRemover(string input)
+            {
+                if (input == null)
+                {
+                    return null;
+                }
+                return input.TrimStart().TrimEnd();
+            }
+            public static string StringReverser(string input)
+            {
+                if (input == null)
+                {
+                    return null;
+                }
+                char[] charArray = input.ToCharArray();
+                Array.Reverse(charArray);
+                return new string(charArray);
+            }
+        }
+    }
+
+
