d9f91cf423782d608c75a076fa04304ee0bebe9a
Rahul-agrawal11 <agrawalrahul11112004@gmail.com>
2025-11-26 11:41:23 +0530

    Add files via upload

---
+++ UsingPrivateAssembly/UsingPrivateAssembly/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PrivateAssembly;
+
+namespace UsingPrivateAssembly
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Class1.SpaceRemover("   Rahul   "));
+            Console.WriteLine(Class1.StringReverser("RahulAgrawal"));
+        }
+    }
+}
