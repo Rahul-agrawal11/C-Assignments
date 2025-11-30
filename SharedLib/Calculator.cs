0b5ac37e4c566a83f391e707ec56378ff8610628
Rahul-agrawal11 <agrawalrahul11112004@gmail.com>
2025-11-30 21:41:41 +0530

    Shared Assembly

---
+++ SharedLib/Calculator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedLib
+{
+    public class Calculator
+    {
+        public static int Add(int a, int b) { return a + b; }
+
+        public static int Subtract(int a, int b) {if (a > b) return a - b; return b - a; }
+
+        public static int Multiply(int a, int b) {return a * b; }
+
+        public static int Divide(int a, int b) { if (a > b) return a / b; return b / a; }
+    }
+}
