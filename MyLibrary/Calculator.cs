afff932fcc11eed126fa0139ab0fc780ebd162de
Rahul-agrawal11 <agrawalrahul11112004@gmail.com>
2025-11-30 21:39:18 +0530

    Add files via upload
    Building a simple calculator using Private Assembly.

---
+++ MyLibrary/Calculator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLibrary
+{
+    public class Calculator
+    {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Subtract(int a, int b)
+        {
+            if (a < b) return b - a;
+            return a - b;
+        }
+
+        public static int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        public static int Division(int a, int b)
+        {
+            if (a < b) return b / a;
+            return a / b;
+        }
+    }
+}
