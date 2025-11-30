30985bfbf4f07db64a55f7ed414b7ceb44bdb8af
Rahul-agrawal11 <agrawalrahul11112004@gmail.com>
2025-11-30 21:42:44 +0530

    Using Calculator class of Shared Assembly from GAC

---
+++ UsingSharedLib/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SharedLib;
+
+namespace UsingSharedLib
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = Calculator.Add(5, 3);
+            Console.WriteLine(result);
+        }
+    }
+}
